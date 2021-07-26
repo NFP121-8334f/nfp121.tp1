5c2b335483a741f0892af4085bbbf882129b2420
Nardine <nardinefakhreddine@gmail.com>
2021-07-26 16:31:18 +0300

    done

---
+++ tp1/question3/AuditeurCNAM.java
@@ -0,0 +1,118 @@
+package question3;
+
+/**
+ * NFP121 TpIntroduction, usage de BlueJ et du "Submitter".
+ * 
+ * @version septembre 2009
+ * @author à compléter
+ * @see java.lang.String, java.lang.Math
+ */
+public class AuditeurCNAM {
+    /** l'attribut nom de chaque auditeur. */
+    private String nom;
+    /** l'attribut prenom de chaque auditeur. */
+    private String prenom;
+    /** l'attribut matricule de chaque auditeur. */
+    private String matricule;
+
+    /**
+     * "Création", le constructeur d'un auditeur avec son nom, son prénom et son
+     * matricule.
+     * 
+     * @param nom
+     *            le nom de l'auditeur
+     * @param prenom
+     *            son prénom
+     * @param matricule
+     *            sur la carte d'inscription, près de la photo
+     */
+    public AuditeurCNAM(String nom, String prenom, String matricule) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.matricule = matricule;
+    }
+
+    /**
+     * le login au Cnam : 6 premières lettres du nom suivies de la première
+     * lettre du prénom séparées de '_' . le login retourné est en minuscules,
+     * le trait d'union, ou spéciaux <i>(pour unix)</i> sont remplacés par des
+     * '_' pas de caractères accentués pour le login voir les classes
+     * prédéfines, java.lang.String : les méthodes replaceAll, toLowerCase et
+     * substring java.lang.Math : la méthode min<br>
+     * <b>BlueJ : Menu Aide</b>
+     * 
+     * @return le login du Cnam simplifié, sans les adaptations dues aux
+     *         homonymes...
+     */
+    public String login() {
+        String n;
+        String p;
+        //utilisation de Math.min pour eviter StringIndexOutOfBoundsException
+        n = this.nom.substring(0,Math.min(this.nom.length(),6));
+        //premiere lettre du prénom
+        p = this.prenom.substring(0,1);
+        //minuscule
+        n = n.toLowerCase();
+        p = p.toLowerCase();
+        //faire une méthode pour les accents
+        n = pasAccents(n);
+        p = pasAccents(p);
+        n = n.replaceAll("[^a-z]","_");
+        p = p.replaceAll("[^a-z]","_");
+        return n+"_"+p;
+    }
+
+    /**
+     * Lecture du nom de l'auditeur.
+     * 
+     * @return son nom
+     */
+    public String nom() {
+        return this.nom;
+    }
+    /**
+     * Lecture du prénom de l'auditeur.
+     * 
+     * @return son prénom
+     */
+    public String prenom() {
+        return this.prenom;
+    }
+      /**
+    * Méthode pour enlever les accents
+    * Soumettre un String avec accents
+    * @return String sans accents
+    */
+    public String pasAccents(String x)
+    { //Manque surement des accents
+    String accents = "àâäãéèêëîïìöôòõüûùÀÂÄÃÉÈÊËÎÏÌÖÔÒÕÜÛÙ"; //tableau regroupant les accents
+    String paccents = "aaaaeeeeiiioooouuuaaaaeeeeiiioooouuu";//tableau ayant les mêmes lettres mais sans accents
+    int l = accents.length();//longueur du tableau
+    for(int i = 0; i<l ; i++)
+    {
+    x = x.replace(accents.charAt(i), paccents.charAt(i));
+    }
+    return x;
+    }
+    /**
+     * Lecture du matricule de l'auditeur.
+     * 
+     * @return son matricule
+     */
+    public String matricule() {
+        return this.matricule;
+    }
+
+    /**
+     * méthode toString ( méthode redéfinie de la classe Object).
+     * 
+     * @return la concaténation du nom, du prénom et du login, selon cette
+     *         syntaxe
+     *         <code>nom() + " " + prenom() +  " login : " + login()</code>
+     */
+    @Override
+    public String toString() {
+        return nom() + " " + prenom() + " login : " + login();
+    }
+
+}
